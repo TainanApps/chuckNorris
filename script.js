bb5454fbe21941362ac0735ea2de74330cd1f57c
Robbie Hudek <hudektech@gmail.com>
2022-01-19 12:36:11 +0800

    all categories

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const jokeCategory = document.querySelector("#joke-category");
 const jokeText = document.querySelector("#joke-text");
 const btnGetJoke = document.querySelector("#joke-get");
 const chuckCategories = "https://api.chucknorris.io/jokes/categories";
+const chuckAll = "https://api.chucknorris.io/jokes/random";
 const chuckJokes = "https://api.chucknorris.io/jokes/random?category=";
 
 // const chuckJokes = 'https://api.chucknorris.io/jokes/random?category={category}'
@@ -20,15 +21,22 @@ const loadCategories = async () => {
   let dropDown = document.querySelector("#joke-category");
   dropDown.options.innerHTML = "";
 
+  // Clear out the old
   [...dropDown.options].map((e) => e.remove());
 
+  // Place the all category
+  let item = document.createElement("option");
+  item.innerHTML = `<option class='items' value="all">All</option>`;
+  dropDown.options.add(item);
+
+  // Add in the specified categories
   for (let i = 0; i < respJson.length; i++) {
     let item = document.createElement("option");
     item.innerHTML = `<option class='items' value="${respJson[i]}">${respJson[i]}</option>`;
     dropDown.options.add(item);
   }
 
-  dropDown.selectedIndex = 8;
+  dropDown.selectedIndex = 0;
 
   // call up the first joke
   getNextJoke("dummy");
@@ -37,12 +45,18 @@ const loadCategories = async () => {
 loadCategories();
 
 const getNextJoke = async (e) => {
+  debugger;
   // get the category
   let dropDown = document.querySelector("#joke-category");
   let num = dropDown.selectedIndex;
   let value = [...dropDown.options][num].value;
 
-  respJson = await getJson(`${chuckJokes}${value}`);
+  if (num == 0) {
+    respJson = await getJson(`${chuckAll}`);
+  } else {
+    respJson = await getJson(`${chuckJokes}${value}`);
+  }
+
   jokeText.innerText = respJson.value;
 };
 
